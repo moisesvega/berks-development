825bc987da9a63d6b7d13ed72508b1180ceab1a6
Moises Vega <moises.vega@ooyala.com>
2013-07-29 13:27:35 -0700

    Add structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# berks-development cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: berks-development
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
